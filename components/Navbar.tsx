9f4cfdeb0c12e860413ff68c1694f2cb385118d4
Abdelrahman Hesham <abdo.heshamfathala@gmail.com>
2025-09-08 23:16:29 +0300

    added  auth fixed some bugs

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import {checkUser } from "../lib/checkUser";
+function Navbar(){
+
+    const user = checkUser();
+    return(
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+export default Navbar
